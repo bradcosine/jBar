36afcf3c8b3b003c27cfb72997e7e40f833a7e32
toddmotto <create@toddmotto.com>
2012-12-13 13:26:39 +0000

    Update jbar.js

--- jbar.js
+++ jbar.js
@@ -13,7 +13,7 @@
 		// jBar Defaults
 		var defaults = {
 			type             : 'fixed', // Fixed/Static
-            delay            : '1000', // In milliseconds
+			delay            : '1000', // In milliseconds
             backgroundColor  : '#DB5903', // Background Color
             borderColor      : '#FFF', // Background Color
             buttonTextColor  : '#FFF', // Button Text
@@ -46,12 +46,12 @@
 					  + '</span></div>' + '<span class="jribbon"><img src="img/arrow-down.png" class="arrow jtrigger" alt="Arrow Down"></span>';
 
 	    // jBar Styles
-	    var styles = '<style>' + '.jbar{background:' + backgroundColor + ';top:0;left:0;right:0;z-index:999998;color:#FFF;border-bottom:3px solid ' + borderColor + ';display:none;}'
-	    		   + '.jbar-cta{display:block;max-width:1280px;margin:0 auto;padding:5px;}'
-	    		   + '.jribbon{padding:5px 2px 0;z-index:999999;top:0;right:4%;display:none;width:38px;text-align:center;background:#DF5A02;border:3px solid #FFF;'
+	    var styles = '<style>' + '.jbar{background:' + backgroundColor + ';top:0;left:0;right:0;height:46px;z-index:999998;color:#FFF;border-bottom:3px solid ' + borderColor + ';display:none;}'
+	    		   + '.jbar-cta{display:block;max-width:1280px;margin:0 auto;padding:14px;}'
+	    		   + '.jribbon{padding:5px 2px 0;z-index:999999;top:0;right:4%;display:none;width:38px;text-align:center;background:' + backgroundColor + ';border:3px solid #FFF;'
 	    		   + 'border-top:none;box-shadow:0 2px 5px #333;color:#FFF;cursor:pointer;border-radius:0 0 3px 3px;}' 
-	    		   + '.downarrow{top:11px;right:4%;width:50px;text-align:center;position:absolute;}' + '.jribbon:hover{background:#DF5A02;cursor:pointer;}' 
-	    		   + '.text{display:block;padding:7px 0;font-weight:900;text-align:center;}' + '.text a{font-weight:500;text-decoration:none;color:' 
+	    		   + '.downarrow{top:11px;right:4%;width:50px;text-align:center;position:absolute;margin:0;padding:0;}' + '.jribbon:hover{cursor:pointer;}' 
+	    		   + '.text{display:block;padding:0;margin:0;font-weight:900;text-align:center;}' + '.text a{font-weight:500;text-decoration:none;color:' 
 	    		   + buttonTextColor + ';margin:0 10px;padding:5px 10px;background:' + buttonColor + ';border-radius:3px;-webkit-border-radius:3px;-moz-border-radius:3px;}' 
 	    		   + '.text a:hover{background:' + buttonColorHover + ';}.arrow{cursor:pointer;}.fixed{position:fixed;}.static{position:absolute;}</style>';
 	    	
@@ -67,12 +67,9 @@
 			// Add class 'position-top-fixed'
 			$('#jbar').addClass('position-top fixed');
 			$('.jribbon').addClass('fixed');
-		
-			// Get jBar height for animation height
-			var height = $('#jbar').outerHeight();
 			
 			// Initial Animation
-	        $('body').prepend('<div id="jbar-push" style="height:'+ height +'px;display:none;" />');
+	        $('body').prepend('<div id="jbar-push" style="height:46px;display:none;" />');
 	        $('#jbar, #jbar-push').delay(delay).slideDown(300);
 	        
 	        // User Animation
@@ -90,9 +87,6 @@
 			// Add class 'position-top-fixed'
 			$('#jbar').addClass('position-top');
 			$('.jribbon').addClass('static');
-			
-			// Get jBar height for animation height
-			var height = $('#jbar').outerHeight();
 			
 			// Initial Animation
 	        $('#jbar').delay(delay).slideDown(300);
